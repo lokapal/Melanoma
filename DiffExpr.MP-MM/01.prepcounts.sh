2c8071d9067343b95ffd8562898bd9007e186135
lokapal <lokapal@gmail.com>
2023-03-10 01:05:00 +0300

    Create 01.prepcounts.sh

---
+++ DiffExpr.MP-MM/01.prepcounts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+featureCounts -a /usr/local/genomes/hg38.gtf -o counts.txt -T 30 -t exon -g gene_id \
+../MP/rnaseq.MP.rep1.bam ../MP/rnaseq.MP.rep2.bam \
+../MM/rnaseq.MM.rep1.bam ../MM/rnaseq.MM.rep2.bam
