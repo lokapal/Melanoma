bb0b852927dd8e09c3a2d852f30fffd79c064946
lokapal <lokapal@gmail.com>
2023-03-10 01:05:28 +0300

    Update 01.prepcounts.sh

--- DiffExpr.MP-MM/01.prepcounts.sh
+++ DiffExpr.MP-MM/01.prepcounts.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Dependencies: featureCounts      https://subread.sourceforge.net/
 featureCounts -a /usr/local/genomes/hg38.gtf -o counts.txt -T 30 -t exon -g gene_id \
 ../MP/rnaseq.MP.rep1.bam ../MP/rnaseq.MP.rep2.bam \
 ../MM/rnaseq.MM.rep1.bam ../MM/rnaseq.MM.rep2.bam
