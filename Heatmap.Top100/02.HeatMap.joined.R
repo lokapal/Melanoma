f5b5713ea1cf0759c2c45e96016a3241156d59e4
lokapal <lokapal@gmail.com>
2023-03-15 00:10:28 +0300

    Update 02.HeatMap.joined.R

--- Heatmap.Top100/02.HeatMap.joined.R
+++ Heatmap.Top100/02.HeatMap.joined.R
@@ -11,69 +11,51 @@
 # Common R libraries:
 # 3. dendextend, circlize
 
-
 suppressPackageStartupMessages(library(circlize))
 suppressPackageStartupMessages(library(ComplexHeatmap))
 suppressPackageStartupMessages(library(dendextend))
 
 #GeneID	log2FC	pvalue	padj	Gene
- 
 data100 <- read.table("MP-MM.joined", sep = "\t", header=T, colClasses = c("character", "numeric", "numeric", "numeric", "character", "numeric", "numeric", "numeric"), row.names=NULL)
 
 dimarr <- nrow(data100)
-hei <- dimarr*6/70
+hei <- dimarr*6/70     # here we calculate the row height for the heatmap
 cairo_pdf(filename = "MM-MMI.joined.pdf",
 width = 3, height = hei, pointsize = 12,
 family = "sans", bg = "white", antialias = "default", fallback_resolution = 300, onefile=T)
-#png("test2.png", 300, 1200, pointsize=20)
 
-###!!!!!!!heatmap.2(cbind(data100$Exp, data100$Exp), dendrogram="none",labCol="",labRow=data100$Gene,trace="none")!!!!! 
-###WORKS!!!!
-
-#mat<-cbind(data100$T,data100$NK)
 mat <- cbind(data100$log2FC1, data100$log2FC2)
 rownames(mat) <- data100$Gene
 colnames(mat) <- c("MM-MMI","MP-MM")
+
 row_dend = as.dendrogram(hclust(dist(mat)))
-#row_dend = as.dendrogram(hclust(dist(mat),method = "complete")) ward.D2, mcquitty, median, centroid
 row_dend = color_branches(row_dend, k = 4)
 dend_thin <- row_dend %>%
      set ("branches_lwd",0.75)
 
-row_dend_complete <- as.dendrogram(hclust(dist(mat),method = "complete")) # ward.D2, mcquitty, median, centroid
+row_dend_complete <- as.dendrogram(hclust(dist(mat),method = "complete"))
 row_dend_complete <- color_branches(row_dend_complete, k = 4)
 dend_thin_complete <- row_dend_complete %>%
      set ("branches_lwd",0.75)
 
-row_dend_ward <- as.dendrogram(hclust(dist(mat),method = "ward.D2")) # ward.D2, mcquitty, median, centroid
+row_dend_ward <- as.dendrogram(hclust(dist(mat),method = "ward.D2"))
 row_dend_ward <- color_branches(row_dend_ward, k = 4)
 dend_thin_ward <- row_dend_ward %>%
      set ("branches_lwd",0.75)
 
-row_dend_mcquitty <- as.dendrogram(hclust(dist(mat),method = "mcquitty")) # ward.D2, mcquitty, median, centroid
-#row_dend_mcquitty <- color_branches(row_dend_mcquitty, k = 4)
+row_dend_mcquitty <- as.dendrogram(hclust(dist(mat),method = "mcquitty"))
 dend_thin_mcquitty <- row_dend_mcquitty %>%
      set ("branches_lwd",0.75)
 
-row_dend_median <- as.dendrogram(hclust(dist(mat),method = "median")) # ward.D2, mcquitty, median, centroid
-#row_dend_median <- color_branches(row_dend_median, k = 4)
+row_dend_median <- as.dendrogram(hclust(dist(mat),method = "median"))
 dend_thin_median <- row_dend_median %>%
      set ("branches_lwd",0.75)
 
-row_dend_centroid <- as.dendrogram(hclust(dist(mat),method = "centroid")) # ward.D2, mcquitty, median, centroid
-#row_dend_centroid <- color_branches(row_dend_centroid, k = 4)
+row_dend_centroid <- as.dendrogram(hclust(dist(mat),method = "centroid"))
 dend_thin_centroid <- row_dend_centroid %>%
      set ("branches_lwd",0.75)
 
-#    top_annotation = HeatmapAnnotation(
-#        foo = 1:10,
-#        annotation_legend_param = list(foo = list(title = "foo_top_anno"))
-#    ))
-
 f1 = colorRamp2(c(-5, -2, -1.5, 0, 1.5, 2, 5.5), c("darkblue", "blue", "lightblue", "white", "yellow", "red", "darkred"))
-
-hc <- hclust(dist(mat))
-group <- cutree(hc, k = 20)
 
 Heatmap (mat,
 row_names_side="left",
@@ -92,6 +74,7 @@ heatmap_legend_param = list(
     ),
 ) 
 
+# re-sort the list by MP-MM, instead of MM-MMI
 mat.sorted<-mat[order(mat[,2], decreasing = TRUE), ]
 
 Heatmap (mat.sorted,
@@ -113,12 +96,8 @@ heatmap_legend_param = list(
 Heatmap (mat,
 row_names_side="left",
 heatmap_width = unit(5, "cm"),
-#cluster_rows = TRUE,
-#cluster_rows = row_dend,
 cluster_rows = dend_thin,
-#cluster_rows = cluster_within_group(t(mat), group), 
 row_split = 4,
-#border = TRUE,
 border_gp = gpar(col = "black", lwd = 0.5), ## new version reqd!
 show_column_dend = FALSE,
 row_dend_side = "right",
@@ -176,8 +155,6 @@ Heatmap (mat,
 row_names_side="left",
 heatmap_width = unit(5, "cm"),
 cluster_rows = dend_thin_mcquitty,
-#row_split = 4,
-#border_gp = gpar(col = "black", lwd = 0.5), ## new version reqd!
 show_column_dend = FALSE,
 row_dend_side = "right",
 cluster_columns = FALSE,
@@ -195,8 +172,6 @@ Heatmap (mat,
 row_names_side="left",
 heatmap_width = unit(5, "cm"),
 cluster_rows = dend_thin_median,
-#row_split = 4,
-#border_gp = gpar(col = "black", lwd = 0.5), ## new version reqd!
 show_column_dend = FALSE,
 row_dend_side = "right",
 cluster_columns = FALSE,
@@ -214,8 +189,6 @@ Heatmap (mat,
 row_names_side="left",
 heatmap_width = unit(5, "cm"),
 cluster_rows = dend_thin_centroid,
-#row_split = 4,
-#border_gp = gpar(col = "black", lwd = 0.5), ## new version reqd!
 show_column_dend = FALSE,
 row_dend_side = "right",
 cluster_columns = FALSE,
